a3cee2b772c7722f7f93e8c7e8ec32f6c724db15
Ivan Ganev <iganevru@gmail.com>
2019-11-06 22:59:37 +0300

    lang

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -9,18 +9,26 @@ export default () => {
       sx={{
         color: 'white',
         backgroundImage: `url(${imgForBanner})`,
-        backgroundPosition: '50% 90%',
-        padding: '20px',
+        backgroundPosition: ['50% 90%'],
+        padding: ['20px', '20px'],
         backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% auto',
+        backgroundSize: ['auto 100%', '100% auto'],
         backgroundColor: '#161616',
         borderRadius: '12px',
         boxShadow: '0 2px 8px #15151594',
-        marginBottom: '40px',
-        textShadow: '0 0px 8px #161616'
+        marginBottom: ['20px', '40px'],
+        textShadow: '0 0px 8px #161616',
+        textAlign: ['center', 'left']
       }}
     >
-      <div sx={{ display: 'flex', alignItems: 'center' }}>
+      <div
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: ['center', 'flex-start'],
+          flexWrap: 'wrap'
+        }}
+      >
         <div
           sx={{
             boxShadow: 'inset 0 0 5px #151515',
@@ -32,11 +40,11 @@ export default () => {
             backgroundPosition: '50% 50%',
             borderRadius: '100%',
             backgroundImage: `url(${imgIvanGanev})`,
-            marginRight: '20px'
+            marginRight: ['10px', '20px']
           }}
         ></div>
         <div>
-          <h1 sx={{ fontSize: '40px', my: '0px' }}>Ivan Ganev</h1>
+          <h1 sx={{ fontSize: ['36px', '40px'], my: '0px' }}>Ivan Ganev</h1>
           <p sx={{ marginTop: '0px', fontSize: '20px' }}>web developer</p>
         </div>
       </div>
