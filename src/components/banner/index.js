8a0f48fb6225d4c4d6dcdb90a59a10592eef7a32
Ivan Ganev <iganevru@gmail.com>
2019-11-06 09:33:29 +0300

    banner

---
+++ src/components/banner/index.js
@@ -0,0 +1,54 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import imgForBanner from './2green-chameleon-unsplash-newImage-100.jpeg';
+import imgIvanGanev from './IvanGanev4.jpg';
+
+export default ({
+  name = 'Ivan Ganev',
+  langLink = 'https://ru.ganev.ru/',
+  lang = 'на русском'
+}) => {
+  return (
+    <div
+      sx={{
+        color: 'white',
+        backgroundImage: `url(${imgForBanner})`,
+        backgroundPosition: '50% 90%',
+        padding: '20px',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100% auto',
+        backgroundColor: '#161616',
+        borderRadius: '12px',
+        boxShadow: '0 2px 8px #15151594',
+        marginBottom: '40px'
+      }}
+    >
+      <div sx={{ display: 'flex', alignItems: 'center' }}>
+        <div
+          sx={{
+            boxShadow: 'inset 0 0 5px #151515',
+            height: '151px',
+            width: '151px',
+            minHeight: '151px',
+            minWidth: '151px',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: '50% 50%',
+            borderRadius: '100%',
+            backgroundImage: `url(${imgIvanGanev})`,
+            marginRight: '20px'
+          }}
+        ></div>
+        <div>
+          <h1 sx={{ fontSize: '40px', my: '0px' }}>{name}</h1>
+          <p sx={{ marginTop: '0px', fontSize: '20px' }}>
+            {/* <span sx={{ fontSize: '13px' }}>🌐 </span>
+            <a sx={{ color: 'white' }} href={langLink}>
+              {lang}
+            </a> */}
+            web developer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
